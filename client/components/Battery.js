49f461b8e8f4fedc5ce1235e43b4976509ab27e6
Jack Rimell <jackrimell123@gmail.com>
2017-03-02 22:20:15 +0000

    set up full charge state

--- client/components/Battery.js
+++ client/components/Battery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
 
 class Battery extends Component {
 
@@ -7,10 +8,23 @@ class Battery extends Component {
   }
 
   render() {
+
+    const level = parseInt(this.props.level * 100);
+
+    const classes = cx({
+      'Battery_contents': true,
+      'Battery_contents--full': level > 70,
+      'Battery_contents--empty': level < 20
+    });
+
     return (
       <div className="Battery">
-        <div className="Battery_contents" style={{width: `${parseInt(this.props.level * 100)}%`}}>
-
+        <div className={classes} style={{width: `${level}%`}}>
+          <div className="Battery_face">
+            <div className="Battery_face-left-eye"></div>
+            <div className="Battery_face-right-eye"></div>
+            <div className="Battery_face-mouth"></div>
+          </div>
         </div>
       </div>
     );
