0b20aba646c540fa948f822c54519b0095726b77
Jack Rimell <jackrimell123@gmail.com>
2017-03-02 18:34:25 +0000

    updating styling and moving navigator state to top level component

--- client/components/App.js
+++ client/components/App.js
@@ -5,12 +5,37 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      level: 0.0
+    };
+  }
+
+  componentDidMount() {
+    navigator.getBattery().then(battery => {
+      // Add the battery data to the state
+      this.setState(battery);
+
+      // Listen to when the level of the battery changes
+      battery.addEventListener('levelchange', () => {
+        this.setState(battery);
+      });
+
+      // Listen to when the status of charging changes
+      battery.addEventListener('chargingchange', () => {
+        this.setState(battery);
+      });
+
+    });
   }
 
   render() {
     return (
-      <div className="app">
-        <Battery />
+      <div className="App">
+        <div className="App_wrapper">
+          <Battery {...this.state} />
+          <p className="Battery_level">{parseInt(this.state.level * 100)}%</p>
+        </div>
       </div>
     );
   }
--- client/components/Battery.js
+++ client/components/Battery.js
@@ -4,34 +4,14 @@ class Battery extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      level: 0.0
-    };
-  }
-
-  componentDidMount() {
-    navigator.getBattery().then(battery => {
-      // Add the battery data to the state
-      this.setState(battery);
-
-      // Listen to when the level of the battery changes
-      battery.addEventListener('levelchange', () => {
-        this.setState(battery);
-      });
-
-      // Listen to when the status of charging changes
-      battery.addEventListener('chargingchange', () => {
-        this.setState(battery);
-      });
-
-    });
   }
 
   render() {
     return (
       <div className="Battery">
-        <p>{this.state.level * 100}%</p>
+        <div className="Battery_contents" style={{width: `${parseInt(this.props.level * 100)}%`}}>
+
+        </div>
       </div>
     );
   }
